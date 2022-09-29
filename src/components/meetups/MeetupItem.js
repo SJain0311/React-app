02844769de8380698678113635d9a97af33165b7
SJain0311 <simranjain031196@gmail.com>
2022-09-29 18:28:58 +0530

    Third Commit

--- src/components/meetups/MeetupItem.js
+++ src/components/meetups/MeetupItem.js
@@ -1,8 +1,25 @@
-import React from 'react'
+import React,{useContext} from 'react'
 import classes from './MeetupItem.module.css'
 import Card from '../ui/Card'
+import FavoritesContext from '../../store/favorites-context';
 
 const MeetupItem = (props) => {
+    const favoritesCtx = useContext(FavoritesContext);
+    const itemIsFavorite = favoritesCtx.itemIsFavorite(props.id);
+    const toggleFavoriteStatusHandler=()=> {
+        if (itemIsFavorite) {
+          favoritesCtx.removeFavorite(props.id);
+        } else {
+          favoritesCtx.addFavorite({
+            id: props.id,
+            title: props.title,
+            description: props.description,
+            image: props.image,
+            address: props.address,
+          });
+        }
+      }
+    
   return (<div>
     <li className={classes.item}>
         <Card>
@@ -16,9 +33,9 @@ const MeetupItem = (props) => {
                 <p>{props.description}</p>
             </div>
             <div className={classes.actions}>
-                <button>
-                    To Favorites
-                </button>
+            <button onClick={toggleFavoriteStatusHandler}>
+            {itemIsFavorite ? 'Remove from Favorites' : 'To Favorites'}
+          </button>
             </div>
         </div>
         </Card>
--- src/pages/NewMeetup.js
+++ src/pages/NewMeetup.js
@@ -1,11 +1,29 @@
-import NewMeetupForm from '../components/meetups/NewMeetupForm';
-import React from 'react';
+import NewMeetupForm from "../components/meetups/NewMeetupForm";
+import React from "react";
+import {useNavigate} from "react-router-dom"
 
 function NewMeetup() {
+  const navigate=useNavigate();
+  function addMeetHandler(meetupData) {
+    fetch(
+      "https://react-getting-app-a5937-default-rtdb.firebaseio.com/meetup.json",
+      {
+        method: "POST",
+        body: JSON.stringify(meetupData),
+        meaders: {
+          "Content-Type": "application/json",
+        },
+      }
+    ).then(()=>
+    {
+      navigate('/')
+    })
+  }
+
   return (
     <section>
       <h1>Add New Meetup</h1>
-      <NewMeetupForm />
+      <NewMeetupForm onAddMeetup={addMeetHandler} />
     </section>
   );
 }
